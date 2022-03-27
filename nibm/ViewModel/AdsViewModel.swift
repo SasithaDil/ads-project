bc7dc08e079d98357b8b0c6893312d75a3f33fcf
Sasitha Dilshan <33307043+SasithaDil@users.noreply.github.com>
2022-03-27 16:18:09 +0530

    wrap with task function

--- nibm/ViewModel/AdsViewModel.swift
+++ nibm/ViewModel/AdsViewModel.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import Firebase
 import FirebaseFirestoreSwift
 
+
 class AdsViewModel: ObservableObject{
     @Published var ads: [AdPost]?
     
@@ -18,24 +19,26 @@ class AdsViewModel: ObservableObject{
     @Published var createAd = false
     @Published var isWriting = false
     
+    
     func fetchAdPosts() async {
-        do{
-            let db = Firestore.firestore().collection("Ads")
-            
-            let ads = try await db.getDocuments()
-            
-            DispatchQueue.main.async {
-                self.ads = ads.documents.compactMap({ ad in
+        Task {
+            do{
+                let db = Firestore.firestore().collection("Ads")
+                
+                let ads = try await db.getDocuments()
+                
+               self.ads = ads.documents.compactMap({ ad in
                     return try? ad.data(as: AdPost.self)
                 })
+                
             }
-            
+            catch{
+                
+                self.alertMessage = error.localizedDescription
+                showAlert.toggle()
+            }
         }
-        catch{
-            
-            self.alertMessage = error.localizedDescription
-            showAlert.toggle()
-        }
+        
     }
     func deleteAd (adPost: AdPost){
         
@@ -49,17 +52,17 @@ class AdsViewModel: ObservableObject{
     }
     func writeAd(content: [adContent], sellerName: String, adTitle: String){
         do{
-
+            
             withAnimation {
                 isWriting = true
             }
-
+            
             let ad = AdPost(title: adTitle, seller: sellerName, adContent: content, date: Timestamp(date: Date()))
-
+            
             let _ = try Firestore.firestore().collection("Ads").document().setData(from: ad)
-
+            
             withAnimation {
-
+                
                 ads?.append(ad)
                 isWriting = true
                 createAd = false
